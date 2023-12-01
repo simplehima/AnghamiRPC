c5f6da92e9fbaac1c6a016527d8d1d5651d8dfa8
ZiAD <pronnerdev@gmail.com>
2023-12-01 22:55:35 +0400

    Sleep addition [https://github.com/voidZiAD/AnghamiRPC/issues/1]

--- AnghRPC/AnghRPC/Program.cs
+++ AnghRPC/AnghRPC/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 
 namespace AnghRPC
 {
@@ -185,6 +186,8 @@ namespace AnghRPC
                             });
 
                         }
+
+                        Thread.Sleep(500);
 
                     }
 
