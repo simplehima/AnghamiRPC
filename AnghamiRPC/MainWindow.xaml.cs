2539eb3579231659b182cbf8b7ba4c2c124a56f3
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2023-12-02 06:43:59 +0200

    Base working GUI

--- AnghamiRPC/MainWindow.xaml.cs
+++ AnghamiRPC/MainWindow.xaml.cs
@@ -1,24 +1,46 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AnghamiRPC
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread? rpcThread;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            string clientId = ClientIdTextBox.Text;
+            rpcThread = new Thread(() => Program.Start(clientId, UpdateUI));
+            rpcThread.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            Program.Stop();
+        }
+
+        private void UpdateUI(string message)
+        {
+            // Use this method to update UI elements from the background thread
+            Dispatcher.Invoke(() =>
+            {
+                ConsoleLogTextBox.Text += $"{message}\n";
+            });
+        }
+
+        // Ensure to stop the background thread when the application is closing
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            Program.Stop();
+        }
+
+
     }
 }
